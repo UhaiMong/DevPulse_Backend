37bb74e3ca8ecc979932a94e92ade3e06ff990a7
UhaiMong <uhaimarma2016@gmail.com>
2026-05-22 01:20:34 +0600

    Changed into expected login user data

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -3,20 +3,16 @@ import { authService } from "./auth.service";
 
 const loginUser = async (req: Request, res: Response) => {
   try {
-    const result = await authService.loginUserDB(req.body);
-
-    const { refreshToken } = result;
-
-    res.cookie("refreshToken", refreshToken, {
-      secure: false,
-      httpOnly: true,
-      sameSite: "lax",
-    });
+    const { email, password } = req.body;
+    const { token, user } = await authService.loginUserDB({ email, password });
 
     res.status(200).json({
       success: true,
       message: "User login successfully",
-      data: result,
+      data: {
+        token,
+        user,
+      },
     });
   } catch (error: any) {
     res.status(500).json({
@@ -28,26 +24,6 @@ const loginUser = async (req: Request, res: Response) => {
   }
 };
 
-const refreshToken = async (req: Request, res: Response) => {
-  try {
-    const result = await authService.generateRefreshToken(
-      req.cookies.refreshToken,
-    );
-    res.status(200).json({
-      success: true,
-      message: "Refresh Token Generated!!",
-      data: result,
-    });
-  } catch (error: any) {
-    res.status(500).json({
-      success: false,
-      message: `Internal Server Error ${error.message}`,
-      error: error,
-    });
-  }
-};
-
 export const authController = {
   loginUser,
-  refreshToken,
 };
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -4,7 +4,6 @@ import { pool } from "../../config/db";
 import { envConfig } from "../../config/env";
 
 const JWT_SECRET = envConfig.jwt_secret_token;
-const JWT_REFRESH = envConfig.jwt_refresh_token;
 
 const loginUserDB = async (payload: { email: string; password: string }) => {
   const { email, password } = payload;
@@ -27,56 +26,23 @@ const loginUserDB = async (payload: { email: string; password: string }) => {
   }
 
   //   token generation
-
   const jwtPayload = {
     id: user.id,
     name: user.name,
     role: user.role,
     email: user.email,
   };
-  const accessToken = jwt.sign(jwtPayload, JWT_SECRET as string, {
+  const token = jwt.sign(jwtPayload, JWT_SECRET as string, {
     expiresIn: "1d",
   });
-  const refreshToken = jwt.sign(jwtPayload, JWT_REFRESH as string, {
-    expiresIn: "10d",
-  });
-
-  return { accessToken, refreshToken };
-};
-
-const generateRefreshToken = async (token: string) => {
   if (!token) {
-    throw new Error("Unuthorized access!");
+    throw new Error("Forbiden Access");
   }
-  const decodedData = jwt.verify(
-    token as string,
-    JWT_REFRESH as string,
-  ) as JwtPayload;
+  const { password: _, ...userWithoutPassword } = user;
 
-  const userData = await pool.query(
-    `
-        SELECT * FROM users WHERE email = $1
-        `,
-    [decodedData.email],
-  );
-  const user = userData.rows[0];
-
-  if (userData.rows.length === 0) {
-    throw new Error("User Not Found!");
-  }
-  const jwtPayload = {
-    id: user.id,
-    name: user.name,
-    role: user.role,
-    email: user.email,
-  };
-  const accessToken = jwt.sign(jwtPayload, JWT_SECRET as string, {
-    expiresIn: "1d",
-  });
-  return { accessToken };
+  return { token, user: userWithoutPassword };
 };
 
 export const authService = {
   loginUserDB,
-  generateRefreshToken,
 };
